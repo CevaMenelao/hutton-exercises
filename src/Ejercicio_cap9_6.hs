67f19be8fa9b32656cb9f1ef2a68e75e9f159664
CevaMenelao <38533197+CevaMenelao@users.noreply.github.com>
2020-08-27 16:19:08 -0500

    Chapter 11

--- src/Ejercicio_cap9_6.hs
+++ src/Ejercicio_cap9_6.hs
@@ -1,20 +1,21 @@
 module Ejercicio_cap9_6 where
 
 data Op = Add | Sub | Mul | Div | Exp
+          deriving Eq
 
 instance Show Op where
     show Add = "+"
     show Sub = "-"
     show Mul = "*"
     show Div = "/"
-    show Exp = "^"
+    show Exp = "^"                                      --Added for 6_a
     
 apply :: Op -> Int -> Int -> Int
 apply Add x y = x + y
 apply Sub x y = x - y
 apply Mul x y = x * y
 apply Div x y = x `div` y
-apply Exp x y = x ^ y
+apply Exp x y = x ^ y                                   --Added for 6_a
 
 --Sacado de ejercicio 9_9
 valid :: Op -> Int -> Int -> Bool
@@ -22,7 +23,7 @@ valid Add x y = x<=y
 valid Sub x y = x > y
 valid Mul x y = x > 1 && y > 1 && x<=y 
 valid Div x y = y > 1 && x `mod` y == 0
-valid Exp x y = y > 1 && x > 1
+valid Exp x y = y > 1 && x > 1                            --Added for 6_a
 
 --9_3:
 data Expr = Val Int | App Op Expr Expr
@@ -108,14 +109,57 @@ sol=solutions' [1,3,7,10,25,50] 765
 
 -- solutions [1,2,5] 25
 
-
+--Added for 6_c
 easy :: [Expr]->[Expr]
 easy [e] =  [e]
 easy (e:es) = smaller ++ [e] ++ taller
     where 
         smaller = [e' | e'<-es , length (show e')<=length (show e)]
         taller  = [e' | e'<-es , length (show e')>length (show e)]
+{-
+instance Eq Op where
+    --(==) ::a  -> a -> Bool
+    Add == Add = True
+    Sub == Sub = True 
+    Mul == Mul = True  
+    Div == Div = True
+    Exp == Exp = True
+    _   == _   = False
+    --(/=) ::a  -> a -> Bool
+    a /= b = not (a == b)
+-}
 
+contar :: Expr -> Op -> Int
+contar (Val _) _ = 0
+contar (App o e1 e2) p = (contar e1 p) + (contar e2 p) + if p==o then 1 else 0 
+
+menorque :: Expr -> Expr -> Bool  
+menorque e1 e2 = if res >=0 then False else True
+    where 
+        cs1 = reverse (map (contar e1) ops) 
+        cs2 = reverse (map (contar e2) ops)
+        res = foldl (\x y -> 10*x + y) 0 [c1-c2 | (c1,c2) <- zip cs1 cs2]
+
+halve :: [a]->([a],[a])
+halve x = (first, second)
+    where
+        h=(length x) `div` 2
+        first = take h x
+        second = drop h x
+
+merge :: [Expr] -> [Expr] -> [Expr]
+merge [] ys = ys
+merge xs []  = xs
+merge (x:xs) (y:ys) | menorque x y = [x] ++ merge xs (y:ys)
+                    | otherwise      = [y] ++ merge (x:xs) ys
+
+
+msort :: [Expr]->[Expr]
+msort [a]  = [a] 
+msort xs = merge (msort( fst (halve xs)))  (msort(snd (halve xs)))
+
+
+--Added for 6_b
 minimo2 :: [(a,Int)] -> (a,Int)
 minimo2 [(a,n)] = (a,n)
 minimo2 ( (a,n) :xs ) = ss
@@ -125,7 +169,7 @@ minimo2 ( (a,n) :xs ) = ss
 
 --solutions' ns n = [e | ns' <- choices ns, (e,m) <- results ns', m == n]
 solutions' :: [Int] -> Int -> [Expr]
-solutions' ns n = if null s2s then [fst(minimo2 s1s)] else easy s2s
+solutions' ns n = if null s2s then [fst(minimo2 s1s)] else msort s2s
     where   
         s1s = [(e,abs(n-m)) | ns' <- choices ns, (e,m) <- results ns']
         s2s = [e |  (e,m)<-s1s , m == 0] 
--- src/Ejercicios_cap10.hs
+++ src/Ejercicios_cap10.hs
@@ -1,8 +1,7 @@
--- |
 module Ejercicios_cap10 where
 
-import Data.Char
-
+import           Data.Char
+import           System.IO
 --10_4:
 act2 :: IO (Char,Char)
 act2 = do   x <- getChar
@@ -131,3 +130,169 @@ play2 board player =
 
 nim :: IO ()
 nim = play2 initial 1
+
+
+--10_8:
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int,Int)
+
+goto :: Pos -> IO ()
+goto (x,y) = putStr ("\ESC["++ show y ++ ";" ++ show x ++ "H")
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = do   goto p
+                    putStr xs
+
+width :: Int
+width = 10
+
+height :: Int
+height = 10
+
+type Board2 = [Pos]
+
+glider :: Board2
+glider = [(4,2),(2,3),(4,3),(3,4),(4,4)]
+
+showcells :: Board2 -> IO ()
+showcells b = sequence_ [writeat p "O" | p <- b]
+
+isAlive :: Board2 -> Pos -> Bool
+isAlive b p = elem p b
+
+isEmpty :: Board2 -> Pos -> Bool
+isEmpty b p = not (isAlive b p)
+
+wrap :: Pos -> Pos
+wrap (x,y) = (((x-1) `mod` width) +1,
+              ((y-1) `mod` height) +1 )
+
+neighbs :: Pos -> [Pos]
+neighbs (x,y) = map wrap [(x-1,y-1),(x,y-1),
+                          (x+1,y-1),(x-1,y),
+                          (x+1,y),(x-1,y+1),
+                          (x,y+1),(x+1,y+1)]
+
+liveneighbs :: Board2 -> Pos -> Int
+liveneighbs b = length . filter (isAlive b) . neighbs
+
+survivors :: Board2 -> [Pos]
+survivors b = [p | p <- b, elem (liveneighbs b p) [2,3]]
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : rmdups (filter (/= x) xs)
+
+births :: Board2 -> [Pos]
+births b = [p | p <- rmdups (concat (map neighbs b)),
+                isEmpty b p,
+                liveneighbs b p == 3]
+
+nextgen :: Board2 -> Board2
+nextgen b = survivors b ++ births b
+
+wait :: Int -> IO ()
+wait n = sequence_ [return () | _ <- [1..n]]
+
+life :: Board2 -> IO ()
+life b = do     cls
+                showcells b
+                wait 500000
+                life (nextgen b)
+
+-- 10_10_1:
+--putStr2 :: String -> IO ()
+--putStr2 [] = return ()
+--putStr2 (x:xs) = do putChar x
+--                    putStr2 xs
+putStr3 :: String -> IO ()
+putStr3 cs = sequence_ [putChar c | c <- cs ]
+
+
+
+
+-- 10_10_2:
+--putBoard :: Board -> IO ()
+--putBoard [a,b,c,d,e] = do putRow 1 a
+--                          putRow 2 b
+--                          putRow 3 c
+--                          putRow 4 d
+--                          putRow 5 e
+    
+putBoard2 :: Board -> IO ()
+putBoard2 = aux 1
+
+aux :: Int -> Board -> IO ()
+aux _ []        = return ()
+aux n (x:xs)    = do putRow n x
+                     aux (n+1) xs
+
+
+
+
+-- 10_10_3:
+putBoard3 :: Board -> IO ()
+putBoard3 xs = sequence_ [putRow n x | (n,x) <- zip [1..(length xs)] xs ]
+
+
+
+
+-- 10_10_4:
+numsreader :: IO Int
+numsreader = do xs <- getLine 
+--              if all isDigit xs then                    Capitulo 11
+                if and [isDigit x' | x' <- xs] then
+                    return (foldl (\x y -> 10*x + y) 0 (map digitToInt xs))
+                else 
+                    do putStrLn "ERROR: Invalid number, try again"
+                       numsreader
+
+sumator :: Int -> IO Int
+sumator 0 = return (0)
+sumator n = do x1 <- numsreader
+               x2 <- sumator (n-1)
+               return (x1 + x2)
+
+adder :: IO ()
+adder = do putStr "How many numbers? "
+           nums <- numsreader
+           sol <- sumator nums
+           putStrLn ("The total is " ++ show sol)
+
+
+
+
+-- 10_10_5:
+adder2 :: IO ()
+adder2 = do putStr "How many numbers? "
+            nums <- numsreader
+            sol <- sequence [numsreader | _ <-[1..nums]]
+            putStrLn ("The total is " ++ show (sum sol))
+
+
+
+-- 10_10_6:
+getCh :: IO Char
+getCh = do hSetEcho stdin False
+           x <- getChar
+           hSetEcho stdin True
+           return x
+
+readLine :: IO String
+readLine = do x <- getCh
+              if x == '\n' then
+                  do putChar x
+                     return []
+              else
+                  if x == '\DEL' then
+                      do putChar '\b'
+                         xs <- readLine
+                         return xs
+                  else
+                      do putChar x
+                         xs <- readLine
+                         return (x:xs)
+
